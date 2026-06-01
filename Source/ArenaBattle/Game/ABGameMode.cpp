fb0f042c3e71f8a76ad71191fd672d3c9a015649
ronniej <ronniej@naver.com>
2026-06-01 15:46:53 +0900

    5강 완료 - Repliated/ReplicateUsing / 블루프린트 RepNotify까지 진행

--- Source/ArenaBattle/Game/ABGameMode.cpp
+++ Source/ArenaBattle/Game/ABGameMode.cpp
@@ -31,63 +31,63 @@ void AABGameMode::OnPlayerDead()
 
 }
 
-void AABGameMode::PreLogin(
-	const FString& Options,
-	const FString& Address,
-	const FUniqueNetIdRepl& UniqueId,
-	FString& ErrorMessage)
-{
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("=========================="));
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
-
-	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
-
-	// ErrorMessage에 아무런 값을 입력하지 않으면 로그인을 통과시킴.
-	// ErrorMessage에 값이 입력되면 오류로 간주 -> 접속 차단.
-	//ErrorMessage = TEXT("Server is full");
-
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
-}
-
-void AABGameMode::PostLogin(APlayerController* NewPlayer)
-{
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
-
-	Super::PostLogin(NewPlayer);
-
-	// 클라이언트 정보 출력.
-	UNetDriver* NetDriver = GetNetDriver();
-	if (NetDriver)
-	{
-		// 클라이언트의 접속이 없는 경우.
-		if (NetDriver->ClientConnections.Num() == 0)
-		{
-			AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No Client Connection"));
-		}
-		// 클라이언트의 접속이 있는 경우.
-		else
-		{
-			for (const auto& Connection : NetDriver->ClientConnections)
-			{
-				AB_LOG(LogABNetwork, Log, TEXT("Client Connection : %s"),
-					*Connection->GetName()
-				);
-			}
-		}
-	}
-	else
-	{
-		AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No NetDriver"));
-	}
-
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
-}
-
-void AABGameMode::StartPlay()
-{
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
-
-	Super::StartPlay();
-
-	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
-}
+//void AABGameMode::PreLogin(
+//	const FString& Options,
+//	const FString& Address,
+//	const FUniqueNetIdRepl& UniqueId,
+//	FString& ErrorMessage)
+//{
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT//("=========================="));
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+//
+//	Super::PreLogin(Options, Address, UniqueId, ErrorMessage);
+//
+//	// ErrorMessage에 아무런 값을 입력하지 않으면 로그인을 통과시킴.
+//	// ErrorMessage에 값이 입력되면 오류로 간주 -> 접속 차단.
+//	//ErrorMessage = TEXT("Server is full");
+//
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+//}
+//
+//void AABGameMode::PostLogin(APlayerController* NewPlayer)
+//{
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+//
+//	Super::PostLogin(NewPlayer);
+//
+//	// 클라이언트 정보 출력.
+//	UNetDriver* NetDriver = GetNetDriver();
+//	if (NetDriver)
+//	{
+//		// 클라이언트의 접속이 없는 경우.
+//		if (NetDriver->ClientConnections.Num() == 0)
+//		{
+//			AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No Client //Connection"));
+//		}
+//		// 클라이언트의 접속이 있는 경우.
+//		else
+//		{
+//			for (const auto& Connection : NetDriver->ClientConnections)
+//			{
+//				AB_LOG(LogABNetwork, Log, TEXT("Client Connection : %s"),
+//					*Connection->GetName()
+//				);
+//			}
+//		}
+//	}
+//	else
+//	{
+//		AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("No NetDriver"));
+//	}
+//
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+//}
+//
+//void AABGameMode::StartPlay()
+//{
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("Begin"));
+//
+//	Super::StartPlay();
+//
+//	AB_LOG(LogABNetwork, Log, TEXT("%s"), TEXT("End"));
+//}
--- Source/ArenaBattle/Game/ABGameMode.h
+++ Source/ArenaBattle/Game/ABGameMode.h
@@ -20,13 +20,13 @@ public:
 
 	virtual void OnPlayerDead() override;
 
-	virtual void PreLogin(
-		const FString& Options, 
-		const FString& Address, 
-		const FUniqueNetIdRepl& UniqueId, 
-		FString& ErrorMessage) override;
-
-	virtual void PostLogin(APlayerController* NewPlayer) override;
-
-	virtual void StartPlay() override;
+	//virtual void PreLogin(
+	//	const FString& Options, 
+	//	const FString& Address, 
+	//	const FUniqueNetIdRepl& UniqueId, 
+	//	FString& ErrorMessage) override;
+	//
+	//virtual void PostLogin(APlayerController* NewPlayer) override;
+	//
+	//virtual void StartPlay() override;
 };
