30f0b8300b0e22bf816f68abb03cfc3d12023efd
ronniej <ronniej@naver.com>
2026-06-08 16:03:46 +0900

    움직임 리플리케이션 관련 옵션 및 로컬에서 이동 막기 추가

--- Source/ArenaBattle/Character/ABCharacterPlayer.cpp
+++ Source/ArenaBattle/Character/ABCharacterPlayer.cpp
@@ -223,6 +223,12 @@ void AABCharacterPlayer::SetCharacterControlData(const UABCharacterControlData* 
 
 void AABCharacterPlayer::ShoulderMove(const FInputActionValue& Value)
 {
+	// 공격 중에는 이동하지 않도록 막기.
+	if (!bCanAttack)
+	{
+		return;
+	}
+
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
 	const FRotator Rotation = Controller->GetControlRotation();
@@ -245,6 +251,12 @@ void AABCharacterPlayer::ShoulderLook(const FInputActionValue& Value)
 
 void AABCharacterPlayer::QuaterMove(const FInputActionValue& Value)
 {
+	// 공격 중에는 이동하지 않도록 막기.
+	if (!bCanAttack)
+	{
+		return;
+	}
+
 	FVector2D MovementVector = Value.Get<FVector2D>();
 
 	float InputSizeSquared = MovementVector.SquaredLength();
